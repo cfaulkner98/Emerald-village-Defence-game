0d8aea583a3cd13a541496b5e25e138a5da00413
cfaulkner98 <faulkner.chloe@icloud.com>
2024-12-03 19:35:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ pg.init()
 # create clock 
 clock = pg.time.Clock()
 
-
 #game window
 screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 pg.display.set_caption("Emerald Village Defence")
 
 #load images
 enemy_image = pg.image.load('images/enemy_1.png').convert_alpha()
+
 #game loop 
 run = True 
 while run:
