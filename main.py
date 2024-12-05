eb760924ea80a04bb153dffaf6ee9615185f25dc
cfaulkner98 <faulkner.chloe@icloud.com>
2024-12-05 12:04:26 +0000

    waypoint
    allowed for my enemy to follow the path and added a background using tiled

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame as pg 
+import json
 from enemy import Enemy
+from world import World
 import constants as c 
 
 #initialise game 
@@ -13,19 +15,24 @@ screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 pg.display.set_caption("Emerald Village Defence")
 
 #load images
+#map
+map_image = pg.image.load('map/level.png').convert_alpha()
+#enemies
 enemy_image = pg.image.load('images/enemy_1.png').convert_alpha()
+
+#load json data for level
+with open('map/waypoints.json') as file:
+   world_data = json.load(file)
+
+#create world
+world = World(world_data, map_image)
+world.process_data()
 
 #create groups 
 enemy_group = pg.sprite.Group()
 
-waypoints = [
-   (100, 100),
-   (400, 200),
-   (400, 100), 
-   (200, 300),
-]
 
-enemy = Enemy((waypoints), enemy_image)
+enemy = Enemy((world.waypoints), enemy_image)
 enemy_group.add(enemy)
 
 #game loop 
@@ -34,11 +41,10 @@ while run:
 
     clock.tick(c.FPS)
 
-    screen.fill("grey100") 
+    screen.fill("grey100")
 
-    #draw enemy path
-    pg.draw.lines(screen, "grey0", False, waypoints)
-                
+    #draw level
+    world.draw(screen) 
 
     #update groups
     enemy_group.update()
